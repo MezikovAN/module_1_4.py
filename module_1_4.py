47882b20713078eb9dd3720223d5efd7cffb433a
MezikovAN <malek31334@gmail.com>
2024-10-12 13:02:19 +0300

    Add files via upload
     Организуйте программу:
    
    Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
    Вывести количество символов введённого текста
    3. Работа с методами строк:
    
    Используя методы строк, выполните следующие действия:
    
    Выведите строку my_string в верхнем регистре.
    Выведите строку my_string в нижнем регистре.
    Измените строку my_string, удалив все пробелы.
    Выведите первый символ строки my_string.
    Выведите последний символ строки my_string.

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите ваш текст: ')
+print ('Количество символов введеного в вашем тексте :' , len(my_string ))
+print ('Текст в верхнем регистре : ', my_string.upper())
+print ('Текст в нижнем регистре :', my_string .lower())
+print ('Без всех пробелов  :', my_string .replace(' ' , ''))
+print (my_string[0]) # вывести первый символ
+print (my_string[-1]) # вывести последний  символ
